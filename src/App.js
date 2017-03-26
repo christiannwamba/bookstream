d8b989dfaebeffebbb2a32c6b033279663d453b7
Christian Nwamba <nwambachristian@gmail.com>
2017-03-26 17:03:03 +0100

    Initial

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import Nav from './components/Nav/Nav'
+import BookList from './components/BookList/BookList'
+import data from './data'
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+      super(props)
+      this.state = {
+          books: data,
+          cart: []
+      }
+
+      this.handleAddToCart = this.handleAddToCart.bind(this);
+
+  }
+
+  handleAddToCart(book) {
+      console.log(book);
+      const cartItem = this.state.cart.find(x => x.id === book.id);
+      const newBooks = this.state.books.map(x => {
+          if(x.id === book.id) {
+              return Object.assign({}, x, {inStock: x.inStock - 1});
+          }
+          return x;
+      });
+      !cartItem && this.setState({cart: [...this.state.cart, book]}, () => {
+          console.log(newBooks);
+          this.setState({books: newBooks})
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Nav cart={this.state.cart}/>
+        <div className="App-main">
+            <BookList
+                books={this.state.books}
+                handleAddToCart={this.handleAddToCart}
+            ></BookList>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/BookItem/BookItem.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import './BookItem.css';
+
+class BookItem extends Component {
+    render() {
+        const book = this.props.book;
+        return (
+            <div className="BookItem">
+                <div className="BookItem__img"
+                    style={{
+                        backgroundImage: `url(${book.image})`,
+                        width: '800px',
+                        height: 300,
+                        backgroundSize: 'contain',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'top left'
+                    }}
+                >
+                    <img
+                        src={book.image}
+                        alt="Book Item"
+                    />
+                </div>
+                <div className="BookItem__details">
+                    <div className="BookItem__cat">
+                        {book.category}
+                    </div>
+                    <h4>{book.title}</h4>
+                    <h5>{book.author}</h5>
+                    <div className="BookItem__price-cart">
+                        <p>${book.price}</p>
+                        <button
+                            onClick={this.props.handleClick.bind(this, book)}
+                        ><span className="fa fa-cart-plus"></span> Buy</button>
+                    </div>
+                    <div className="BootItem__description">
+                        {book.description}
+                    </div>
+                    <div className="BookItem__stock" style={{color: book.inStock >= 5 ? '#417505' : '#CE0814'}}>
+                        {book.inStock} In Stock
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BookItem;
